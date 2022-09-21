f037249e4226c228817037dfec3679e507841a59
Luiz Ricardo <luizricardobarros@gmail.com>
2022-09-20 22:12:30 -0300

    rotas children

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,8 +6,15 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path:'',
-    component:HomeComponent
-  }
+    component:HomeComponent,
+    children:[
+      {
+        path:'',
+        component:LoginComponent
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
