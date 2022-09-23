688a967f3a8806623d8593b6e34997671fe138f8
Luiz Ricardo <luizricardobarros@gmail.com>
2022-09-22 21:54:01 -0300

    ngForm e login e autenticação

---
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  constructor(private httpCliente:HttpClient) { }
+
+  autenticar(usuario:string,senha:string):Observable<any>{
+    return this.httpCliente.post('http://localhost:3000/user/login',{
+      userName: usuario,
+      password:senha
+    })
+  }
+}
--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,30 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario: string = '';
+  senha: string = '';
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      (u) => {
+        this.router.navigate(['animais']);
+      },
+      (error) => {
+        console.log('Usuário ou senha inválido');
+      }
+    );
   }
-
 }
